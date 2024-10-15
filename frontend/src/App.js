bf48deeb0e5675b99fcf59b2eb58d723ab152b1a
yadnyesh8250 <yadnyeshpatil8250@gmail.com>
2024-10-15 23:36:32 +0530

    the Home page extend [change the images ]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,11 @@ import Home from "./pages/Home";
 import {  Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Login from "./pages/Login";
+import Admission from "./pages/Admission"
 import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
 import VerifyEmail from "./pages/VerifyEmail";
-import { Verify } from "crypto";
+//import { Verify } from "crypto";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verify" element={ <VerifyEmail/> } />
+        <Route path="admission" element={<Admission/>}/>
       </Routes>
       <Toaster position="bottom-right" reverseOrder={false} />
     </>
